8041eca11297d966e1e2feafd154c7325c784a9d
donrestarone <shashikejayatunge@gmail.com>
2018-04-04 09:20:00 -0400

    finished calculate pv method in tvom, again needs a lot of testing. works for now

--- updated-version/tvom.rb
+++ updated-version/tvom.rb
@@ -6,8 +6,6 @@ class Tvom
 		@interest_rate = interest_rate 
 		@years = years 
 		@compounding_frequency = compounding_frequency
-
-		
 	end 
 
 	def view_calculation_name #reader
@@ -56,7 +54,7 @@ class Tvom
 		
 	end
 
-	def calculate_annuity(calculation_name, interest_rate, years, compounding_frequency, future_value) #here calculation name is the annuity payment
+	def calculate_fv_annuity(calculation_name, interest_rate, years, compounding_frequency, future_value) #here calculation name is the annuity payment
 		n = years * compounding_frequency
 		interest_factor = ( 1 + interest_rate.to_f / 100 ) 
 		interest_decimal = (interest_rate.to_f / 100)
@@ -64,6 +62,12 @@ class Tvom
 		return fv
 	end 
 
+	def calculate_pv_annuity(calculation_name, interest_rate, years, compounding_frequency, future_value) #here calculation name is the annuity payment
+		n = years * compounding_frequency
+		interest_factor = ( 1 + interest_rate.to_f / 100 ) ** n 
+		interest_decimal = (interest_rate.to_f / 100)
+		pv = calculation_name * ( (1 - (1 / interest_factor) ) / interest_decimal)
+	end
 end 
 
 # class tvom
@@ -91,4 +95,8 @@ puts tvom.calculate_pv("test", 9, 3, 1, 25000).inspect
 
 puts 
 puts 
-puts tvom.calculate_annuity(10000, 11, 2, 1, 0).inspect
+puts tvom.calculate_fv_annuity(10000, 11, 2, 1, 0).inspect
+
+puts 
+puts 
+puts tvom.calculate_pv_annuity(10000, 11, 2, 1, 0).inspect
