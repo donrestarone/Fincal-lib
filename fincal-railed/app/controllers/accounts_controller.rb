35c7bc2b6cb3098bfcf3dceebe974aeaedbfcc2f
donrestarone <shashikejayatunge@gmail.com>
2018-04-15 15:50:25 -0400

    create account works under /accounts/new

--- fincal-railed/app/controllers/accounts_controller.rb
+++ fincal-railed/app/controllers/accounts_controller.rb
@@ -7,14 +7,28 @@ class AccountsController < ApplicationController
 	end 
 
 	def create
-	
+		@account = Account.new
+		@account.account_name = params[:account][:account_name]
+		@account.category = params[:account][:category]
+		@account.balance = params[:account][:balance]
+		@account.interest = params[:account][:interest]
+		@account.compounding_frequency = params[:account][:compounding_frequency]
+		@account.compounding_periods = params[:account][:compounding_periods]
+		
+		if @account.save
+			redirect_to '/accounts'
+		else 
+			render :new
+		end
 	end 
 
 	def new 
-
+		@account = Account.new 
 	end 
 
 	def edit
+		id = params[:id]
+		@account = Account.find(id)
 
 	end 
 
