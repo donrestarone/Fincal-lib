730f857ac226f65d1c50e1203e18af3643919edc
donrestarone <shashikejayatunge@gmail.com>
2018-04-16 20:48:12 -0400

    add dynamic routes for show

--- fincal-railed/app/controllers/accounts_controller.rb
+++ fincal-railed/app/controllers/accounts_controller.rb
@@ -33,14 +33,29 @@ class AccountsController < ApplicationController
 	end 
 
 	def show 
-
+		@account = Account.find(params[:id])
 	end 
 
 	def update
+		@account = Account.find(params[:id])
 
+		@account.account_name = params[:account][:account_name]
+		@account.category = params[:account][:category]
+		@account.balance = params[:accounts][:balance]
+		@account.interest = params[:accounts][:interest]
+		@account.compounding_frequency = params[:accounts][:compounding_frequency]
+		@account.compounding_periods = params[:accounts][:compounding_periods]
+
+		if @account.save
+			redirect_to '/accounts'
+		else 
+			render :new
+		end 
 	end 
 
 	def destroy
-
+		@account = Account.find(params[:id])
+		@account.destroy
+		redirect_to accounts_path
 	end 
 end
