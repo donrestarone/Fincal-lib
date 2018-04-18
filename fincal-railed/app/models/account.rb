ab89564fd3ea43f72311b9ab9e750d56d92caa6d
donrestarone <shashikejayatunge@gmail.com>
2018-04-17 21:22:22 -0400

    minor tweaks to the views

--- fincal-railed/app/models/account.rb
+++ fincal-railed/app/models/account.rb
@@ -1,16 +1,5 @@
 class Account < ApplicationRecord
-	validates :name, length: {in: 4..20}, presence: true, uniqueness: true
+	validates :name, length: {in: 4..40}, presence: true, uniqueness: true
 	validates :category, presence: true
 end
 
-
-# create_table "accounts", force: :cascade do |t|
-#     t.string "name"
-#     t.string "category"
-#     t.float "balance"
-#     t.float "interest"
-#     t.integer "compounding_frequency"
-#     t.integer "compounding_periods"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
-#   end
