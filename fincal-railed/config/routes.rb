ade2282a151941c3d986d76a43d0382eb93a147a
donrestarone <shashikejayatunge@gmail.com>
2018-04-22 15:26:16 -0400

    nested accounts routes under users

--- fincal-railed/config/routes.rb
+++ fincal-railed/config/routes.rb
@@ -2,26 +2,34 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 
-# Resource Routes  
-	resources :users, only: %i(create update destroy)
-  	resources :sessions, only: %i(create)
+# # Resource Routes  
+# 	resources :users, only: %i(create update destroy)
+#   	resources :sessions, only: %i(create)
 
-  	#Manuall Routes
-  	#Main Routes
-  	root 'main#index'
+#   	#Manuall Routes
+#   	#Main Routes
+#   	root 'main#index'
 
-  	#User Routes
-  	get '/user/profile' => 'users#profile'
-  	get '/login' => 'users#login'
-  	get '/signup' => 'users#new'
-    post '/user/updatepassword' => 'users#changePassword'
+#   	#User Routes
+#   	get '/user/profile' => 'users#profile'
+#   	get '/login' => 'users#login'
+#   	get '/signup' => 'users#new'
+#     post '/user/updatepassword' => 'users#changePassword'
 
-    #Sessions Routes
-    get 'session/destroy' => 'sessions#destroy'
+#     #Sessions Routes
+#     get 'session/destroy' => 'sessions#destroy'
 
-  root 'main#index'
-  get '/tvom' => 'accounts#tvom'
-  resources :accounts 
+#   root 'main#index'
+#   get '/tvom' => 'accounts#tvom'
+#   resources :accounts 
+
+resource :users, only: %i(create update destroy) do
+  resources :accounts
+end
+
+resource :sessions, only: [:new, :create, :destroy]
+
+
 
 
 end
