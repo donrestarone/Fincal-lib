8935c24902f91eccf912cef276f258523d5860f7
donrestarone <shashikejayatunge@gmail.com>
2018-04-24 13:15:19 -0400

    make changes to the create user account page that have password conditions and placeholder text in the fields

--- fincal-railed/app/models/user.rb
+++ fincal-railed/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 	has_secure_password
 
 	PASSWORD_FORMAT = /\A
-	  (?=.{6,})          # Must contain 8 or more characters
+	  (?=.{6,})          # Must contain 6 or more characters
 	  (?=.*\d)           # Must contain a digit
 	  (?=.*[a-z])        # Must contain a lower case character
 	  (?=.*[A-Z])        # Must contain an upper case character
