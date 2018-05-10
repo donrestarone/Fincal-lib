41ad69ae6008e98c4ee3392f7feefac1ae4db022
donrestarone <shashikejayatunge@gmail.com>
2018-05-09 23:26:59 -0400

    refactor code in account model

--- fincal-railed/app/models/account.rb
+++ fincal-railed/app/models/account.rb
@@ -5,31 +5,26 @@ class Account < ApplicationRecord
 	validates :category, presence: true
 
 	def calculate_fv
-		interest_factor = (1 + (interest.to_f / 100) / compounding_frequency)
-		fv = balance * interest_factor ** compounding_periods
+		fv = balance * interest_factor(interest, compounding_frequency) ** compounding_periods
 		rounded_fv = fv.round(2)
 		return rounded_fv
 	end
 
 	def calculate_pv
-		interest_factor = ( 1 + (interest.to_f / 100) / compounding_frequency )
-		pv = (balance) / interest_factor ** compounding_periods
+		pv = (balance) / interest_factor(interest, compounding_frequency) ** compounding_periods
 		rounded_pv = pv.round(2)
 		return rounded_pv
 	end
 
 	def calculate_fv_annuity
-		interest_factor = ( 1 + (interest.to_f / 100) / compounding_frequency )
-		interest_decimal = (interest.to_f / 100) / compounding_frequency
-		fv = balance * (interest_factor.to_f ** compounding_periods - 1) / interest_decimal.to_f
+		fv = balance * (interest_factor(interest, compounding_frequency) ** compounding_periods - 1) / interest_decimal(interest, compounding_frequency)
 		rounded_fv = fv.round(2)
 		return rounded_fv
 	end
 
 	def calculate_pv_annuity
 		interest_factor = ( 1 + (interest.to_f / 100) / compounding_frequency ) ** compounding_periods
-		interest_decimal = (interest.to_f / 100) / compounding_frequency
-		pv = balance * ( (1 - (1 / interest_factor) ) / interest_decimal)
+		pv = balance * ( (1 - (1 / interest_factor) ) / interest_decimal(interest, compounding_frequency))
 		rounded_pv = pv.round(2)
 		return rounded_pv
 	end
@@ -49,6 +44,18 @@ class Account < ApplicationRecord
 		denominator = interest_factor_minus_one / interest_equalized.to_f
 		numerator = balance / denominator
 		rounded_annuity = numerator.round(2)
+	end
+
+	private
+
+	def interest_factor(interest, compounding_frequency)	#refactor this to use the method below
+		interest_factor = (1 + (interest.to_f / 100) / compounding_frequency)
+		return interest_factor
+	end
+
+	def interest_decimal(interest, compounding_frequency)
+		decimal = (interest.to_f / 100) / compounding_frequency
+		return decimal
 	end
 
 end
