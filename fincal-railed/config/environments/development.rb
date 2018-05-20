0dacf33fc1ff5f35cc6ad00089c2a27abc12e0c1
donrestarone <shashikejayatunge@gmail.com>
2018-05-20 15:15:52 -0400

    action mailer setup

--- fincal-railed/config/environments/development.rb
+++ fincal-railed/config/environments/development.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   
   #settings for gmail
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
