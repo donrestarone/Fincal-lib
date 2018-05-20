2039472c5a5094556a88e63c68dd32cd2f0a73a9
donrestarone <shashikejayatunge@gmail.com>
2018-05-20 14:20:53 -0400

    start work on action mailer, method and views

--- fincal-railed/app/controllers/users_controller.rb
+++ fincal-railed/app/controllers/users_controller.rb
@@ -14,13 +14,16 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	# If user get saved to database
-  	if @user.save
-      session[:user_id] = @user.id
-  		redirect_to users_accounts_path
-  	else
-  	#If there is error while saving to database
-  		render :new
-  	end
+
+    	if @user.save
+        session[:user_id] = @user.id
+        
+        UserMailer.with(user: @user).welcome_email.deliver_now
+    		redirect_to users_accounts_path
+    	else
+    	#If there is error while saving to database
+    		render :new
+    	end
 
   end
 
