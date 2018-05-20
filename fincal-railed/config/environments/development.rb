58fbac0c72e99ebe2c89d42fa70c71e621fe93db
donrestarone <shashikejayatunge@gmail.com>
2018-05-20 16:22:09 -0400

    finalize mailer for production

--- fincal-railed/config/environments/development.rb
+++ fincal-railed/config/environments/development.rb
@@ -29,18 +29,18 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   
   #settings for gmail
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV['GMAIL_USERNAME'],
-    :password => ENV['GMAIL_PASSWORD'],
-    :authentication => "plain",
-    :enable_starttls_auto => true
-  }
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :user_name => ENV['GMAIL_USERNAME'],
+  #   :password => ENV['GMAIL_PASSWORD'],
+  #   :authentication => "plain",
+  #   :enable_starttls_auto => true
+  # }
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
